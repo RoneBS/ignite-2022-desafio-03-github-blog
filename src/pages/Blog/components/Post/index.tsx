56416e3368817cb216adb8276caaa74608d5fdef
Rone Silveira <ronebs10@hotmail.com>
2022-09-06 15:26:13 -0300

    Home page complete

---
+++ src/pages/Blog/components/Post/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles'
+
+export const Post = () => {
+  return (
+    <S.PostContainer>
+      <div>
+        <strong>JavaScript data types and data structures</strong>
+        <span>Há 1 dia</span>
+      </div>
+      <p>
+        Programming languages all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data structures available in...{' '}
+      </p>
+    </S.PostContainer>
+  )
+}
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,26 @@
+import { Post } from './components/Post'
 import { Profile } from './components/Profile'
 import { SearchInput } from './components/SearchInput'
+
+import * as S from './styles'
 
 export const Blog = () => {
   return (
     <>
       <Profile />
       <SearchInput />
+      <S.PostListContainer>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </S.PostListContainer>
     </>
   )
 }
