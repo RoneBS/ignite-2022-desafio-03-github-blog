4fd9e6114ba115b83b7dbe721a8296e63532ea1a
Rone Silveira <ronebs10@hotmail.com>
2022-09-06 14:46:28 -0300

    SearchInput component added

--- src/pages/Blog/components/Profile/index.tsx
+++ src/pages/Blog/components/Profile/index.tsx
@@ -1,3 +1,6 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ExternalLink } from '../../../../components/ExternalLink'
 import * as S from './styles'
 
@@ -12,6 +15,25 @@ export const Profile = () => {
 
           <ExternalLink text="Github" href="#" />
         </header>
+        <p>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </p>
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+            RoneBS
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBuilding} />
+            Várzea Alegre
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUserGroup} />
+            123 seguidores
+          </li>
+        </ul>
       </S.ProfileDetails>
     </S.ProfileContainer>
   )
---
+++ src/pages/Blog/components/SearchInput/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+export const SearchInput = () => {
+  return (
+    <S.SearchInputContainer>
+      <header>
+        <h3>Publicações</h3>
+        <span>6 publicações</span>
+      </header>
+
+      <input type="text" placeholder="Buscar conteúdo" />
+    </S.SearchInputContainer>
+  )
+}
